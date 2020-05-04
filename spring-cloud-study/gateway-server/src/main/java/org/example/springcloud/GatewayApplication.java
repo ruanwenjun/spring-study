1b3355781a9185f55eca62ee8a3a45ef80c1ce34
ruanwenjun <bestruanwenjun@gmail.com>
2020-05-05 01:49:32 +0800

    add gateway-server

---
+++ spring-cloud-study/gateway-server/src/main/java/org/example/springcloud/GatewayApplication.java
@@ -0,0 +1,18 @@
+package org.example.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author ruanwenjun
+ * @create 2020/05/05/ 1:20 AM
+ */
+@EnableEurekaClient
+@SpringBootApplication
+public class GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+}
